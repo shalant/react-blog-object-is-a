871a6ef8738d283e094ecc698b89083c220c20a3
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-01-31 10:46:04 -0600

    still debugging CSS, 3300

--- src/components/WriteUp.js
+++ src/components/WriteUp.js
@@ -6,22 +6,29 @@ function WriteUp() {
     const CopyWriteRef = useRef();
     const text_threshold = 500;
 
+    function handle_read_more(event) {
+        event.target.parentElement.children[0].style.height = 'fit-content';
+        event.target.parentElement.children[1].style.display = 'none';
+        event.target.parentElement.children[2].style.display = 'none';
+    }
+
     useEffect(() => {
         const copywrite = CopyWriteRef.current;
 
-        // if (copywrite.getBoundingClientRed().height > text_threshold) {
-        //     copywrite.style.height = `${text_threshold}px`;
-        // }
+        if (copywrite.getBoundingClientRect().height > text_threshold) {
+            copywrite.style.height = `${text_threshold}px`;
+            copywrite.nextSibling.style.display = 'block';
+            copywrite.nextSibling.nextSibling.style.display = 'block';
+        }
     })
 
   return (
     <div className='write_up'>
-        <div className='copyright'>
-            lorem30 lorem30 lorem30
-            lorem30 lorem 30 lorem 30 lorem30 lorem 30 lorem 30 lorem30 lorem 30 lorem 30 lorem30 lorem 30 lorem 30 lorem30
+        <div ref={CopyWriteRef} className='copywright'>
+            lorem30 lorem30 lorem30 lorem30 lorem 30 lorem 30 lorem30 lorem 30 lorem 30 lorem30 lorem 30 lorem 30 lorem30 lorem 30 lorem 30 lorem30
         </div>
         <div className='blur'></div>
-        <div className='read_more'>Read more...</div>
+        <div onClick={handle_read_more} className='read_more'>Read more...</div>
     </div>
   )
 }
